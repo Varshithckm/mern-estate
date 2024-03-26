290fbbb64124c5227603914a29192dd099e47766
Varshith R <varshithvarshi8613@gmail.com>
2024-03-26 18:05:26 +0530

    Added pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes , Route} from 'react-router-dom';
+import { Home,About, Profile,SignIn,SignUp } from './pages';
+
 
 function App() {
   return (
-    <div className='text-red-500 '>
-      App
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   )
 }
 
