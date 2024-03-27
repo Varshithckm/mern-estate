c08b07c7e48fa454d995a01389cc46cfde9b5d85
Varshith R <varshithvarshi8613@gmail.com>
2024-03-27 17:54:53 +0530

    Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes , Route} from 'react-router-dom';
 import { Home,About, Profile,SignIn,SignUp } from './pages';
+import {Header }from './components';
 
 
 function App() {
   return (
     <Router>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
